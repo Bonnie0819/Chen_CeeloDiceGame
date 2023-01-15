344c38c4fac57e6e78e216fc4981630b3e5c11bc
Bonnie <bonniec15@nycstudents.net>
2023-01-15 13:15:04 -0500

    Commit

--- src/Die.java
+++ src/Die.java
@@ -2,6 +2,7 @@ public class Die {
     private int die1;
     private int die2;
     private int die3;
+    private int score;
     private int rollTotal;
 
     //getter methods
@@ -34,6 +35,24 @@ public class Die {
         die1 = (int) (Math.random() * 6) + 1;
         die2 = (int) (Math.random() * 6) + 1;
         die3 = (int) (Math.random() * 6) + 1;
+    }
+
+    //calculates the score of the dice
+    public int score() {
+        if(die1 == die2) {
+            score = die3;
+            return score;
+        } else if(die1 == die3) {
+            score = die2;
+            return score;
+        } else if(die2 == die3) {
+            score = die1;
+            return score;
+        } else {
+            score = die1 + die2 + die3;
+            return score;
+        }
+
     }
 
 }
