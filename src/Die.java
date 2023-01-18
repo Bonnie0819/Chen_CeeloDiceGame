5ae5e0dd46e2e6e716dd88e0da2bbc423a90b747
student <bonniec15@nycstudents.net>
2023-01-18 10:12:59 -0500

    Commit

--- src/Die.java
+++ src/Die.java
@@ -36,49 +36,51 @@ public class Die {
         die1 = (int) (Math.random() * 6) + 1;
         die2 = (int) (Math.random() * 6) + 1;
         die3 = (int) (Math.random() * 6) + 1;
-        //ordering the dice from least to greatest
-        if(die1 > die2 && die2 > die3) {
-            int temp = die3;
-            die3 = die1;
-            die1 = temp;
+        order();
+    }
+
+    //checks to see if player/banker wins
+    public int win() {
+        if (die1 == die2 && die2 == die3) {
+            return 0;       // 0 indicates a win
+        } else if (die1 == 4 && die2 == 5 && die3 == 6) {
+            return 0;
+        } else if (die1 == 1 && die2 == 2 && die3 == 3) {
+            return 1;      // 1 indicates a loss
         }
-
-
+        return 2;          // 2 indicates reroll
     }
 
 
     //private helper methods
     //calculates the score of the dice
-    private int score() {
-        if(die1 == die2 && die2 != die3) {
+    private void score() {
+        if (die1 == die2 && die2 != die3) {
             score = die3;
-            return score;
-        } else if(die1 == die3 && die1 != die2) {
+        } else if (die1 == die3 && die1 != die2) {
             score = die2;
-            return score;
-        } else if(die2 == die3 && die2 != die1) {
+        } else if (die2 == die3 && die2 != die1) {
             score = die1;
-            return score;
         } else {
             score = die1 + die2 + die3;
-            return score;
         }
+    }
 
-    //checks to see if player/banker wins
-    private int win() {
-        if (die1 == die2 && die2 == die3) {
-            return 0;       // 0 indicates a win
-        } else if ((die1 == 4 && die2 == 5 && die3 == 6) || (die1 == 5 && die2 == 6 && die3 == 4) || (die1 == 6 && die2 == 4 && die3 ==5)) {
-            return 0;
-        } else if ((die1 == 1 && die2 == 2 && die3 == 3) && (die1 == 2 && die2 == 1 && die3 == 3)) {
-            return 1;      // 1 indicates a loss
+
+        //orders the dice from least to greatest
+        private void order() {
+            int max = Math.max(die1, Math.max(die2, die3));
+            int min = Math.min(die1, Math.min(die2, die3));
+            int mid = die1 + die2 + die3 - max - min;
+
+            die1 = min;
+            die2 = mid;
+            die3 = max;
         }
-        return 2;          // 2 indicates reroll
-         }
 
 
 
 
     }
 
-}
+
